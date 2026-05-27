ef4cdfd325aa20a84ed433c7248afb4416a816d5
Shourya Sharma <shouryasharma1203@gmail.com>
2026-05-27 17:27:37 +0530

    finished

--- 01vitereact/src/components/todos.jsx
+++ 01vitereact/src/components/todos.jsx
@@ -1,33 +1,61 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { removeTodo , updateTodo } from '../features/counter/todoSlice'
+import { removeTodo, updateTodo } from '../features/counter/todoSlice'
+import { useState } from 'react'
 function Todos() {
   const select = useSelector(state => state.todo.todos)
   const dispatch = useDispatch()
+  const [editingID, setEditingID] = useState(null)
+  const [editingText, setEditingText] = useState('')
+
+  function handleEditing() {
+
+  }
   return (
     <>
       <h2 className="text-center text-black mb-4 font-medium">
         Todos
       </h2>
 
-      {/* <!-- Todo List --> */}
       <div className="space-y-4">
-        {select.map(todo => {
+        {select?.map(todo => {
+          const isEditing=editingID === todo.id
           return (
             <div key={todo.id} className="bg-zinc-900 rounded flex items-center justify-between px-4 py-3">
-              <span className="text-white">{todo.title}</span>
-              <button
-                className="bg-yellow-500 hover:bg-yellow-600 text-white p-2 rounded transition"
-                onClick={() => dispatch(updateTodo({ id: todo.id, title: todo.title }))}
-              >
-                ✏️
-              </button>
-              <button className="bg-red-500 hover:bg-red-600 p-2 rounded transition" onClick={() => dispatch(removeTodo(todo.id))}>
-                🗑
-              </button>
+              {isEditing ?
+                <input type="text" className="flex-1 bg-slate-900 text-white px-4 py-3 rounded outline-none placeholder:text-gray-400" 
+                value={editingText} onChange={(e)=>setEditingText(e.target.value)}
+                />
+                :
+                <span className="text-white">{todo.title}</span>}
+
+              {isEditing ? 
+              <button className="bg-green-500 hover:bg-green-600 p-2 rounded transition" onClick={() => {
+                dispatch(updateTodo({ id: todo.id, title: editingText }))
+                setEditingID(null)
+                setEditingText("")
+              }}>
+                Save
+              </button> 
+              :
+                <div className="flex gap-2">
+                  <button
+                    className="bg-yellow-500 hover:bg-yellow-600 text-white p-2 rounded transition"
+                    onClick={()=>{
+                      setEditingText(todo.title)
+                      setEditingID(todo.id)}
+                    }
+                  >
+                    ✏️
+                  </button>
+                  <button className="bg-red-500 hover:bg-red-600 p-2 rounded transition" onClick={() => dispatch(removeTodo(todo.id))}>
+                    🗑
+                  </button>
+                </div>}
+
             </div>
           )
-        }
-        )}
+
+        })}
       </div>
     </>
   )
--- 01vitereact/src/features/counter/todoSlice.js
+++ 01vitereact/src/features/counter/todoSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { nanoid } from '@reduxjs/toolkit'
+const savedTodos=localStorage.getItem("my_todos")
 const initialState = {
-  todos:[{id:1, title:'learn redux'}],
+  todos: savedTodos ? JSON.parse(savedTodos) : [{id:1, title:'learn redux'}]
 }
 
 export const todoSlice = createSlice({
@@ -11,15 +12,18 @@ export const todoSlice = createSlice({
     addTodo: (state, action) => {
       const todo={id:nanoid(), title:action.payload}
       state.todos.push(todo)
+      localStorage.setItem("my_todos", JSON.stringify(state.todos))
     },
     removeTodo: (state, action) => {
       state.todos=state.todos.filter(todo=>todo.id!==action.payload)
+      localStorage.setItem("my_todos", JSON.stringify(state.todos))
     },
     updateTodo: (state, action) => {
       const {id , title}=action.payload
       const todo=state.todos.find(todo=>todo.id===id)
       if(todo){
         todo.title=title
+        localStorage.setItem("my_todos", JSON.stringify(state.todos))
       }
     },
   },
